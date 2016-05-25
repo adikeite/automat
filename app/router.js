410cc795521490dbd71d49731783cb943c3c6a22
Anicka <adikeite@gmail.com>
2016-05-25 12:45:45 +0200

    routes

--- app/router.js
+++ app/router.js
@@ -6,6 +6,12 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('test', function() {
+    this.route('create');
+    this.route('run');
+    this.route('dashboard');
+  });
+  this.route('user');
 });
 
 export default Router;
