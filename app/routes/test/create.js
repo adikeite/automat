eb7ab640ef0a4b076367d3d3871a086c94201f5e
Anicka <adikeite@gmail.com>
2016-06-02 14:49:05 +0200

    cancel, save

--- app/routes/test/create.js
+++ app/routes/test/create.js
@@ -3,5 +3,17 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.createRecord('test')
+  },
+
+  actions: {
+    cancel() {
+      console.log('cancel');
+      this.modelFor('test/create').deleteRecord();
+      this.transitionTo('test');
+    },
+    save() {
+      console.log('save');
+      this.transitionTo('test');
+    }
   }
 });
