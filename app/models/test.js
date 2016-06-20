efb204dc1410846258136becfd084218cca71c6e
Miroslav Juhy Juhos <juhy@rezatyrakety.cz>
2016-06-20 14:15:41 +0200

    Added some necessities

--- app/models/test.js
+++ app/models/test.js
@@ -1,10 +1,18 @@
 import Model from 'ember-data/model';
-import attr from 'ember-data/attr';
+import { attr } from 'ember-computed-decorators/ember-data';
+
 
 export default Model.extend({
-  name: attr('string'),
-  suiteName: attr('string'),
-  prerequisities: attr('string'),
-  steps: attr('string')
 
+	@attr('string')
+	name,
+
+	@attr('string')
+	suiteName,
+
+	@attr('string', {defaultValue: ''})
+	prerequisities,
+
+	@attr('string', {defaultValue: ''})
+	steps
 });
