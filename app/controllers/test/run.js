d0c87b0647ec206fe21185e8cb1d4e3b9a3bc6ac
Miroslav Juhy Juhos <juhy@rezatyrakety.cz>
2016-07-18 13:06:46 +0200

    Test selector

--- app/controllers/test/run.js
+++ app/controllers/test/run.js
@@ -1,8 +1,33 @@
 import Ember from 'ember';
+import computed, { oneWay, observes, alias } from 'ember-computed-decorators';
 
 export default Ember.Controller.extend({
+
+	queryParams: ['selectedProject', 'selectedSuites', 'selectedTests'],
+
+	selectedProject: null,
+	selectedSuites: null,
+	selectedTests: null,
+
 	actions: {
-		projectSelect() {},
-		suiteSelect() {}
+		selectProject(project) {
+			this.set('selectedProject', project);
+			this.set('selectedSuites', null);
+			this.set('selectedTests', null);
+		},
+
+		selectSuite(el) {
+			this.set('selectedSuites', jQuery.makeArray($(':selected', el)).map((item) => {
+				return item.value;
+			}));
+			this.set('selectedTests', null);
+
+		},
+
+		selectTest(el) {
+			this.set('selectedTests', jQuery.makeArray($(':selected', el)).map((item) => {
+				return item.value;
+			}));
+		}
 	}
 });
--- app/routes/test/run.js
+++ app/routes/test/run.js
@@ -2,58 +2,44 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
-
-	model() {
-		return Ember.RSVP.hash({
-			selectedProject: undefined,
-			tests: [],
-			suites: [],
-			projects: this.store.findAll('test').then(tests => {
-				return tests.map(test => test.get('project')).uniq().compact();
-			})
-		});
+	queryParams: {
+		selectedProject: {
+			as: 'project',
+			refreshModel: true
+		},
+		selectedSuites: {
+			as: 'suites',
+			refreshModel: true
+		},
+		selectedTests: {
+			as: 'tests',
+			refreshModel: true
+		}
 	},
 
-	afterModel() {
-		this.selectProject(this.modelFor('test/run').projects.objectAt(0));
-	},
+	model(params) {
+		let suites = (params.suites || '').split(',');
 
-	selectProject(project) {
-		let model = this.modelFor('test/run');
+		return new Promise((resolve, reject) => {
+			this.store.findAll('test').then(allTests => {
+				let selectedProject = params.selectedProject || allTests.map(test => test.get('project')).uniq().compact().objectAt(0),
+					suites = allTests.filter(item => item.get('project') === selectedProject).map(test => test.get('suiteName')).uniq().compact(),
+					selectedSuites = params.selectedSuites ? params.selectedSuites.split(',') : suites,
+					tests = allTests.filter(item => (item.get('project') === selectedProject && selectedSuites.includes(item.get('suiteName')))).map(test => test.get('name')).uniq().compact();
 
-		Ember.set(model, 'selectedProject', project);
-
-		this.store.findAll('test').then(tests => {
-			Ember.set(model, 'suites', tests
-				.filter(item => item.get('project') === project)
-				.map(test => test.get('suiteName')).uniq().compact());
+				resolve({
+					selectedProject: selectedProject,
+					selectedSuites: selectedSuites,
+					selectedTests: params.selectedTests ? params.selectedTests.split(',') : tests,
+					projects: allTests.map(test => test.get('project')).uniq().compact(),
+					suites: suites,
+					tests: tests
+				});
+			});
 		});
 	},
 
 	actions: {
-
-		selectProject(project) {
-			this.selectProject(project);
-		},
-
-		selectSuite(el) {
-			let suites = jQuery.makeArray($(':selected', el)).map((index, item) => {
-				return item.value;
-			});
-
-			let model = this.modelFor('test/run');
-			let project = model.selectedProject;
-
-
-			this.store.findAll('test').then(tests => {
-				Ember.set(model, 'tests', tests
-					.filter(item => (item.get('project') === project) && suites.indexOf(item.suite) > -1)
-					.map(test => test.get('name')).uniq().compact());
-			});
-
-			console.log('suite', suites);
-		},
-
 		run() {}
 	}
 });
