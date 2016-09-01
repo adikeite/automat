f7d6c680e7de44d3217e8a0688189a156204f0da
Miroslav Juhy Juhos <juhy@rezatyrakety.cz>
2016-09-01 15:52:30 +0200

    gh-pages update

--- assets/automat.js
+++ assets/automat.js
@@ -3702,7 +3702,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("automat/app")["default"].create({"name":"automat","version":"0.0.0+cb902170"});
+  require("automat/app")["default"].create({"name":"automat","version":"0.0.0+10b8a72b"});
 }
 
 /* jshint ignore:end */
