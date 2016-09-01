928bd143d537c8708645ff9fcf64397fa739b865
Miroslav Juhy Juhos <juhy@rezatyrakety.cz>
2016-09-01 14:40:13 +0200

    Dashbord move to own route

--- app/router.js
+++ app/router.js
@@ -2,21 +2,22 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
+	location: config.locationType,
+	rootURL: config.rootURL
 });
 
 Router.map(function() {
-  this.route('test', function() {
-      this.route('edit', {path: 'edit/:test_id'});
-  });
-  this.route('create');
-  this.route('run');
-  this.route('import');
-  this.route('user');
-  this.route('loading');
-  this.route('tests', function() {});
-  this.route('runner');
+	this.route('test', function() {
+		this.route('edit', {path: 'edit/:test_id'});
+	});
+	this.route('create');
+	this.route('run');
+	this.route('import');
+	this.route('user');
+	this.route('loading');
+	this.route('tests', function() {});
+	this.route('runner');
+	this.route('dashboard');
 });
 
 export default Router;
