601951dff6bd026beffe5f5bbc97a26d8d9947af
Miroslav Juhy Juhos <juhy@rezatyrakety.cz>
2016-09-01 14:51:16 +0200

    Fixed dashboard reloading

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -3,9 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model() {
 		return Ember.RSVP.hash({
-			tests: this.store.findAll('test'),
-			testRun: this.store.findAll('test-run')
+			tests: this.store.findAll('test', {reload: true}),
+			testRun: this.store.findAll('test-run', {reload: true})
 		}).then((data) => {
+			console.log(data.testRun);
+
 			let ids = data.testRun.toArray().map(item => item.get('test.id')).uniq().map((id) => {
 				return this.store.peekRecord('test', id);
 			});
